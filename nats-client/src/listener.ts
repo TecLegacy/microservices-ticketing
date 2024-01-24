48890e444bd21bdb2ab6fc4efb2a5080e9c5e3a8
TecLegacy <kshvkumar.kk2@gmail.com>
2024-01-24 08:41:54 +0530

    Config(Nats listener)
    subOptions->Manual acknowledgement. Feat-> Graceful shutdown, queue--groups

--- nats-client/src/listener.ts
+++ nats-client/src/listener.ts
@@ -1,17 +1,39 @@
-import nats from 'node-nats-streaming';
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear();
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('listen');
-  const subscription = stan.subscribe('ticket:created');
-  subscription.on('message', msg => {
-    console.log(
-      'Received a message [' + msg.getSequence() + '] ' + msg.getData()
-    );
+
+  stan.on('close', () => {
+    console.log('Closing connection');
+    process.exit();
+  });
+
+  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'ticket-queue-group',
+    options
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const sequence = msg.getSequence();
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log('Received a message [' + sequence + '] ' + data);
+    }
+
+    msg.ack();
   });
 });
+
+// gracefully shutdown on interruption & termination
+process.on('SIGTERM', () => stan.close()); // C
+process.on('SIGINT', () => stan.close()); // Restart
